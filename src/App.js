dd353bdc5078796184bb4de8e0016c1fbef0d3b4
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2023-06-02 18:03:12 +0200

    Added routing to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import Home from './pages/home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="gridContainer">
-        <div className="nav"></div>
-        <div className="mainContent"></div>
-    </div>
+    <Router>
+        <div className="gridContainer">
+            <div className="nav"></div> {/* Replace this div with the nav component */}
+            <div className="mainContent">
+                <Routes>
+                    {/* If you want to add your page add it as the Route below and add the name of the page
+                        like this path="/example", also change the element to the component you want to show */}
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </div>
+    </Router>
   );
 }
 
