c72b1e0bc54bc483f225c360a31e1d8c14a70dbe
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-06-05 12:23:03 +0200

    Add: cont of homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import './Home.css'
 import Herobanner from './Herobanner';
 import ServicesMenu from './ServicesMenu';
+import USP from './USP';
 
 const Home = () => {
     return (
         <div className="home">
             <Herobanner />
             <ServicesMenu />
+            <USP/>
         </div>
     );
 }
---
+++ src/pages/home/USP.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as TBIcons from 'react-icons/tb'
+
+const USP = () => {
+  return (
+    <>
+        <div className = "USPContainer">
+            <div className = "flashCardSystem">
+                <TBIcons.TbCards/>
+                <h3 className = "flashHeader"> Flashcard System</h3>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default USP
