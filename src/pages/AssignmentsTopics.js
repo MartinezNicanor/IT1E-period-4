8395dffec29e83dd2ffd8be63b2c15bef52e1370
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-06-08 11:26:03 +0200

    Add: added line background

--- src/pages/AssignmentsTopics.js
+++ src/pages/AssignmentsTopics.js
@@ -5,6 +5,7 @@ import htmlCssVec from '../assets/images/htmlcss.svg'
 import phpVec from '../assets/images/php.svg'
 import javaVec from '../assets/images/java.svg'
 
+
 const AssignmentsTopics = () => {
   return (
     <div>
@@ -36,6 +37,11 @@ const AssignmentsTopics = () => {
             </div>
             <div className = "javaDesc">
                 <p>With our JAVA assignments you can practice JAVA just in time for your next exam!</p>
+            </div>
+            <div className = "svgLine">
+            <svg width="835" height="406" viewBox="0 0 835 406" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.00037 2C3.00037 2 50.3026 90.0624 271.803 70.5624C370.082 61.9102 521.794 17.887 667 42C839 70.5624 834 269.5 831.5 269.5C777.5 442 453.5 383 303 349.5C152.5 316 124 307 77.4999 338C30.9999 369 37.4999 405.5 37.4999 405.5" stroke="#3E76D5" stroke-width="5"/>
+            </svg>
             </div>
         </div>
     </div>
