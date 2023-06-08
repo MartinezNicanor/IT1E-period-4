78e30d8722e447035ea10754dd30237f3754e838
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-06-08 16:28:30 +0200

    Add: finished assignments topics

--- src/pages/AssignmentsTopics.js
+++ src/pages/AssignmentsTopics.js
@@ -16,8 +16,10 @@ const AssignmentsTopics = () => {
         <div className = "assignmentsTopicsContainer">
             <div className = "htmlCssContainer">
                 <img className='htmlvec' src={htmlCssVec} alt='html css logo vector' />
-                <h4 className = "htmlCss">HTML/CSS</h4>
-                <p className = "assignmentsNumber">placeholder</p>
+                <div className = "htmlCssAssignments">
+                    <h4 className = "htmlCss">HTML/CSS</h4>
+                    <p className = "assignmentsNumber">placeholder</p>
+                </div> 
             </div>
             <div className = "htmlCssDesc">
                 <p>Practice HTML and CSS using the provided assignments to ace your tests!</p>
@@ -27,21 +29,25 @@ const AssignmentsTopics = () => {
             </div>
             <div className = "phpContainer">
                 <img className='phpvec' src={phpVec} alt='php logo vector' />
-                <h4 className = "php">PHP</h4>
-                <p className = "assignmentsNumber">placeholder</p>
+                <div className = "phpAssignments">
+                    <h4 className = "php">PHP</h4>
+                    <p className = "assignmentsNumber">placeholder</p>
+                </div>   
             </div>
             <div className = "javaContainer">
                 <img className='javavec' src={javaVec} alt='java logo vector' />
-                <h4 className = "java">JAVA</h4>
-                <p className = "assignmentsNumber">placeholder</p>
+                <div className = "javaAssignments">
+                    <h4 className = "java">JAVA</h4>
+                    <p className = "assignmentsNumber">placeholder</p>
+                </div> 
             </div>
             <div className = "javaDesc">
                 <p>With our JAVA assignments you can practice JAVA just in time for your next exam!</p>
             </div>
             <div className = "svgLine">
-            <svg width="835" height="406" viewBox="0 0 835 406" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3.00037 2C3.00037 2 50.3026 90.0624 271.803 70.5624C370.082 61.9102 521.794 17.887 667 42C839 70.5624 834 269.5 831.5 269.5C777.5 442 453.5 383 303 349.5C152.5 316 124 307 77.4999 338C30.9999 369 37.4999 405.5 37.4999 405.5" stroke="#3E76D5" stroke-width="5"/>
-            </svg>
+                <svg viewBox="0 0 835 498" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.00049 2C3.00049 2 50.3027 90.0624 271.803 70.5624C370.082 61.9102 521.794 17.887 667 42C839 70.5624 834 269.5 831.5 269.5C777.5 442 393 433 303 403C213 373 169.5 342.5 74.0001 386.5C-21.4997 430.5 28.5001 496 28.5001 496" stroke="#3E76D5" stroke-width="5"/>
+                </svg>
             </div>
         </div>
     </div>
