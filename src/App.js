571c8da777e5f26701139bfd9b067bd02916be79
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2023-06-12 10:59:24 +0200

    Finished opened question page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/home/Home';
 import SideBar from "./components/navbar/SideBar";
 import Profile from './pages/profile/Profile';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Question from './pages/forum/Question';
 
 function App() {
     return (
@@ -16,8 +17,8 @@ function App() {
                     {/* If you want to add your page add it as the Route below and add the name of the page
                     like this path="/example", also change the element to the component you want to show */}
                     <Route path="/" element={<Home />} />
-                    <Route path="/" element={<Home />} />
                     <Route path="/profile" element={<Profile />} />
+                    <Route path="/question/:id" element={<Question />} />
                 </Routes>
                 </div>
             </div>
