b9048aef52faf9d3d4988b1c073ca90f6eaf367f
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-06-12 11:18:34 +0200

    Fix: idk what this was but i cant checkout till i commit

--- src/components/navbar/Logo.js
+++ src/components/navbar/Logo.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import logo from '../../assets/images/logo.png';
 import "./logo.css";
+import { Link } from 'react-router-dom';
+import Home from '../../pages/home/Home';
 
 function Logo() {
     return (
         <>
             <div className="logo">
-                <img src={logo} alt="Logo" />
+                < Link to ={<Home />}>
+                    <img src={logo} alt="Logo" />
+                </Link> 
             </div>
         </>
     )
