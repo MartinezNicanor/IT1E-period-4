1bdca8b23d62886a3cea094ea203cb2e8b680e80
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2023-06-13 19:55:27 +0200

    Fixed linking problem that caused error

--- src/components/navbar/Logo.js
+++ src/components/navbar/Logo.js
@@ -2,15 +2,14 @@ import React from 'react'
 import logo from '../../assets/images/logo.png';
 import "./logo.css";
 import { Link } from 'react-router-dom';
-import Home from '../../pages/home/Home';
 
 function Logo() {
     return (
         <>
-            <div className="logo">
-                < Link to ={<Home />}>
-                    <img src={logo} alt="Logo" />
-                </Link> 
+            <div className="logoNavContainer">
+                <Link to="/">
+                <img src={logo} alt="Logo" className="logoNavbar" />
+                </Link>
             </div>
         </>
     )
