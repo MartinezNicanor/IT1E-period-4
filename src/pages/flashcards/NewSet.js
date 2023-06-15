fe165f314fcddd1cb7272e46fd5c61689554bb35
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-06-15 20:50:27 +0200

    Add: started array

--- src/pages/flashcards/NewSet.js
+++ src/pages/flashcards/NewSet.js
@@ -4,14 +4,26 @@ import './NewSet.css'
 const NewSet = () => {
     const cardData = [
         {
-            
+            term: <>
+                    <input className='termInput'
+                        type={"text"}
+                        placeholder="Enter the term here..."
+                    />
+                </>
+        },{
+            definition: <>
+                            <input className='termInput'
+                                type={"textarea"}
+                                placeholder="Enter the definition here..."
+                            />
+                        </>
         }
     ]
 
 
   return (
     <div className='newSetContainer'>
-        
+        {cardData.term}
     </div>
   )
 }
