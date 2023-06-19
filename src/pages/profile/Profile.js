5fa083ce696da6ac7a7a45743f1ce87789268bcc
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2023-06-19 18:18:51 +0200

    Fixed: made the name and email dynamic

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,34 +1,81 @@
 import './Profile.css';
 import Avatar from './components/Avatar';
-import FormProfile from './components/FormProfile';
 import Header from '../../components/header/Header';
+import { useEffect, useState } from 'react';
+import { useAuthContext } from '../../components/hooks/useAuthContext';
 
 const Profile = () => {
-    const nameForm = {
-        title: "Change your name",
-        inputs: ["First Name", "Last Name"],
-        className: ["fname", "lname"]
-    };
-
-    const passwordForm = {
-        title: "Change your password",
-        inputs: ["New password", "Confirm new password"],
-        className: ["password", "confirmPassword"]
-    };
+    const [fname, setFname] = useState('')
+    const [lname, setLname] = useState('')
+    const [email, setEmail] = useState('')
+    const { user } = useAuthContext()
+    
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+          const response = await fetch('https://projectinnovate-it1e-backend-production.up.railway.app/profile', {
+            method: 'GET',
+            headers: {'Authorization': `Bearer ${user.token}`},
+          })
+          const json = await response.json()
+    
+          if (response.ok) {
+            setFname(json.firstName)
+            setLname(json.lastName)
+            setEmail(json.email)
+          }
+        }
+    
+        if (user) {
+          fetchUserDetails()
+        }
+      }, [user])
 
     return ( 
         <div className="profileContainer">
             <Header title = "Profile" />
             <div className="userInfo">
                 <div className="info">
-                    <p className="nameTitle">Random Name</p>
-                    <p className="emailTitle">random-name@email.com</p>
+                    <p className="nameTitle">{ fname } { lname }</p>
+                    <p className="emailTitle">{ email }</p>
                 </div>
                 <Avatar src = "" alt = "" />
             </div>
             <div className="forms">
-                <FormProfile info = {nameForm} />
-                <FormProfile info = {passwordForm} />
+                <form>
+                <p className="inputTitle">Change your name</p>
+                <div className="inputContainer">
+                    <div className="inputs">
+                        <label>First Name:</label>
+                        <input type="text" name="fname" placeholder="First Name" />
+                    </div>
+                </div>
+
+                <div className="inputContainer">
+                    <div className="inputs">
+                        <label>Last Name:</label>
+                        <input type="text" name="lname" placeholder="Last Name" />
+                    </div>
+                </div>
+                <button className="profileButton">Save</button>
+            </form>
+
+            <form>
+                <p className="inputTitle">Change you password</p>
+                <div className="inputContainer">
+                    <div className="inputs">
+                        <label>Current password:</label>
+                        <input type="password" name="currentPassword" placeholder="Current password" />
+                    </div>
+                </div>
+
+                <div className="inputContainer">
+                    <div className="inputs">
+                        <label>New password:</label>
+                        <input type="password" name="newPassword" placeholder="New password" />
+                    </div>
+                </div>
+                <button className="profileButton">Save</button>
+            </form>
             </div>
         </div>
      );
