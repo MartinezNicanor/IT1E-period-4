8d4de8d40090bdd1689747e8f7bf9548ea018a94
IoanMich <ioan.mich@student.nhlstenden.com>
2023-06-19 14:00:25 +0200

    Add: Added code for fetching data from back-end

--- src/pages/progress/ProgressPage.js
+++ src/pages/progress/ProgressPage.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from "../../components/header/Header";
 import './css/progressPage.css'
 import SubHeader from "./components/SubHeader";
 import ProgressBar from "./components/ProgressBar";
 
 function ProgressPage() {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchProgressData = async () => {
+            try {
+                // Fetch progress data with JWT token included in the headers
+                const progressResponse = await fetch('https://projectinnovate-it1e-backend-production.up.railway.app/progress', {
+                    headers: {
+                        Authorization: `Bearer` + localStorage.getItem('token'),
+                    },
+                });
+
+                const progressData = await progressResponse.json();
+                setData(progressData);
+                setIsLoading(false);
+            } catch (error) {
+                console.error('Error fetching progress data:', error);
+            }
+        };
+
+        fetchProgressData();
+    }, []);
+
     return (
         <div className={"progress-container"}>
             <Header title={"Progress"} />
@@ -16,7 +40,7 @@ function ProgressPage() {
             <ProgressBar label={"HTML/CSS"} />
             <ProgressBar label={"Java"} />
         </div>
-    )
+    );
 }
 
 export default ProgressPage;
