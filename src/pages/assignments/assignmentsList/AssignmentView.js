6e8a8ea51dc45a4bb89137d7a0baec1fa51ee9b3
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-06-20 15:49:56 +0200

    fix: connected to the backend

--- src/pages/assignments/assignmentsList/AssignmentView.js
+++ src/pages/assignments/assignmentsList/AssignmentView.js
@@ -5,34 +5,6 @@ import './AssignmentView.css'
 
 const AssignmentView = () => {
 
-    const assignments = [
-        {
-            title: "Assignment 1",
-            question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Dolor purus non enim praesent elementum facilisis leo vel fringilla. Id interdum velit laoreet id. Maecenas sed enim ut sem viverra. Ante metus dictum at tempor commodo ullamcorper a. Tempus imperdiet nulla malesuada pellentesque elit. Aenean sed adipiscing diam donec adipiscing tristique risus. At consectetur lorem donec massa sapien faucibus et molestie. Sed vulputate mi sit amet mauris commodo quis imperdiet massa.",
-            id: 1
-        }, {
-            title: "Assignment 2",
-            question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Dolor purus non enim praesent elementum facilisis leo vel fringilla. Id interdum velit laoreet id. Maecenas sed enim ut sem viverra. Ante metus dictum at tempor commodo ullamcorper a. Tempus imperdiet nulla malesuada pellentesque elit. Aenean sed adipiscing diam donec adipiscing tristique risus. At consectetur lorem donec massa sapien faucibus et molestie. Sed vulputate mi sit amet mauris commodo quis imperdiet massa.",
-            id: 2
-        }, {
-            title: "Assignment 3",
-            question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Dolor purus non enim praesent elementum facilisis leo vel fringilla. Id interdum velit laoreet id. Maecenas sed enim ut sem viverra. Ante metus dictum at tempor commodo ullamcorper a. Tempus imperdiet nulla malesuada pellentesque elit. Aenean sed adipiscing diam donec adipiscing tristique risus. At consectetur lorem donec massa sapien faucibus et molestie. Sed vulputate mi sit amet mauris commodo quis imperdiet massa.",
-            id: 3
-        }, {
-            title: "Assignment 4",
-            question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Dolor purus non enim praesent elementum facilisis leo vel fringilla. Id interdum velit laoreet id. Maecenas sed enim ut sem viverra. Ante metus dictum at tempor commodo ullamcorper a. Tempus imperdiet nulla malesuada pellentesque elit. Aenean sed adipiscing diam donec adipiscing tristique risus. At consectetur lorem donec massa sapien faucibus et molestie. Sed vulputate mi sit amet mauris commodo quis imperdiet massa.",
-            id: 4
-        }, {
-            title: "Assignment 5",
-            question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Dolor purus non enim praesent elementum facilisis leo vel fringilla. Id interdum velit laoreet id. Maecenas sed enim ut sem viverra. Ante metus dictum at tempor commodo ullamcorper a. Tempus imperdiet nulla malesuada pellentesque elit. Aenean sed adipiscing diam donec adipiscing tristique risus. At consectetur lorem donec massa sapien faucibus et molestie. Sed vulputate mi sit amet mauris commodo quis imperdiet massa.",
-            id: 5
-        }, {
-            title: "Assignment 6",
-            question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet suspendisse interdum consectetur libero id faucibus nisl. Dolor purus non enim praesent elementum facilisis leo vel fringilla. Id interdum velit laoreet id. Maecenas sed enim ut sem viverra. Ante metus dictum at tempor commodo ullamcorper a. Tempus imperdiet nulla malesuada pellentesque elit. Aenean sed adipiscing diam donec adipiscing tristique risus. At consectetur lorem donec massa sapien faucibus et molestie. Sed vulputate mi sit amet mauris commodo quis imperdiet massa.",
-            id: 6
-        },
-    ]
-
     return (
         <div className='assignmentListContainer'>
             <Header title="Assignments" />
@@ -41,7 +13,7 @@ const AssignmentView = () => {
                 <p className='selectAssignment'>Select an assignment and start practicing your coding skills now!</p>
             </div>
             <div className='assignmentList'>
-                {assignments && <AssignmentList assignments={assignments} />}
+                <AssignmentList />
             </div>
 
         </div>
--- src/pages/assignments/components/AssignmentList.js
+++ src/pages/assignments/components/AssignmentList.js
@@ -1,16 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../assignmentsList/AssignmentView.css'
-import { AiOutlineArrowRight } from "react-icons/ai";
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import '../assignmentsList/AssignmentView.css';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import { useLocation } from 'react-router-dom';
+import { useAuthContext } from '../../../components/hooks/useAuthContext';
+
+const AssignmentList = () => {
+    const location = useLocation();
+    const [topic, setTopic] = useState('');
+    const [assignments, setAssignments] = useState([]);
+    const { user } = useAuthContext();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (location.pathname === '/assignments/html') {
+            setTopic('HTML/CSS');
+        }
+        else if (location.pathname === '/assignments/php') {
+            setTopic('PHP');
+        }
+        else if (location.pathname === '/assignments/java') {
+            setTopic('JAVA');
+        } else {
+            navigate('/assignments')
+        }
+
+        const fetchAssignments = async () => {
+            const response = await fetch(
+                `https://projectinnovate-it1e-backend-production.up.railway.app/assignments?topic=${topic}`,
+                {
+                    method: 'GET',
+                    headers: { Authorization: `Bearer ${user.token}` },
+                }
+            );
+            const json = await response.json();
+
+            if (response.ok) {
+                setAssignments(json.assignments);
+            }
+        };
+
+        if (user) {
+            fetchAssignments();
+        }
+    }, [location.pathname, topic, user]);
 
 
-
-const AssignmentList = ({ assignments }) => {
     return (
-        <div className='assignmentsList'>
-            {assignments.map(assignment => (
-                <Link to={`/assignments/:topics/${assignment.id}`} key={assignment.id}>
-                    <div className='assignmentBox'>
+        <div className="assignmentsList">
+            {assignments.map((assignment) => (
+                <Link to={`/assignments/${topic}/${assignment.id}`} key={assignment.id}>
+                    <div className="assignmentBox">
                         {assignment.title}
                         <AiOutlineArrowRight />
                     </div>
@@ -18,7 +58,6 @@ const AssignmentList = ({ assignments }) => {
             ))}
         </div>
     );
-}
+};
 
 export default AssignmentList;
-
