b7ae48510ae5f7cd331d1159e597c6e026db5a35
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-06-22 15:52:22 +0200

    Add: backend connection, prev and next buttons work now and loading animation was added

--- src/pages/assignments/openedAssignment/OpenedAssignment.js
+++ src/pages/assignments/openedAssignment/OpenedAssignment.js
@@ -1,44 +1,119 @@
-import React from 'react'
-import Header from '../../../components/header/Header'
-import * as AiIcons from 'react-icons/ai'
-import './OpenedAssignment.css'
-import Timer from '../components/Timer'
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate, useLocation, Link } from 'react-router-dom';
+import { useAuthContext } from '../../../components/hooks/useAuthContext';
+import Header from '../../../components/header/Header';
+import * as AiIcons from 'react-icons/ai';
+import './OpenedAssignment.css';
+import Timer from '../components/Timer';
 
 const OpenedAssignment = () => {
+    const location = useLocation();
+    const { id } = useParams();
+    const { topic } = useParams();
+    const [assignmentId, setAssignmentId] = useState(id);
+    const [assignmentTopic, setAssignmentTopic] = useState('');
+    const [isLoading, setIsLoading] = useState(null);
+    const [error, setError] = useState(null);
+    const { user } = useAuthContext();
+    const [assignment, setAssignment] = useState({});
+    const navigate = useNavigate();
 
-    const assignments = [
-        {
-            title: "Assignment 1",
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sagittis luctus turpis, a tempus velit cursus nec. Donec quis turpis sodales, tincidunt leo eget, imperdiet eros. Curabitur eu placerat urna. Fusce porttitor sem lacinia sem sagittis porttitor. Pellentesque euismod laoreet finibus. Donec ornare dolor vel libero luctus porttitor. Vestibulum mattis et magna sit amet luctus. Morbi fringilla tellus elit, non gravida nulla euismod et. Sed at tellus ultricies dui lobortis aliquam nec sit amet sem. Vivamus at vulputate ipsum. Duis tristique quis nisi at finibus. Nunc ipsum massa, aliquam sed maximus sit amet, volutpat et dolor. Cras sit amet tristique lectus. Nunc molestie lobortis dignissim. Nam finibus, nulla vel malesuada laoreet, neque sem aliquam diam, in consectetur nisl orci quis metus. Duis tristique quis nisi at finibus. Nunc ipsum massa, aliquam sed maximus sit amet, volutpat et dolor. Cras sit amet tristique lectus. Nunc molestie lobortis dignissim. Nam finibus, nulla vel malesuada laoreet, neque sem aliquam diam, in consectetur nisl orci quis metus. Duis tristique quis nisi at finibus. Nunc ipsum massa, aliquam sed maximus sit amet, volutpat et dolor. Cras sit amet tristique lectus. Nunc molestie lobortis dignissim. Nam finibus, nulla vel malesuada laoreet, neque sem aliquam diam, in consectetur nisl orci quis metus.",
-            id: 1
+    useEffect(() => {
+        // Update the assignment topic based on the current location
+        if (location.pathname === `/assignments/html/${id}`) {
+            setAssignmentTopic('HTML/CSS');
+        } else if (location.pathname === `/assignments/php/${id}`) {
+            setAssignmentTopic('PHP');
+        } else if (location.pathname === `/assignments/java/${id}`) {
+            setAssignmentTopic('JAVA');
+        } else {
+            navigate('/assignments');
+            return;
         }
-    ]
+
+        setIsLoading(true);
+        setError(false);
+
+        const fetchAssignment = async () => {
+            try {
+                const response = await fetch(
+                    `https://projectinnovate-it1e-backend-production.up.railway.app/assignments/specificAssignement?topic=${assignmentTopic}&questionNum=${id}`,
+                    {
+                        method: 'GET',
+                        headers: { Authorization: `Bearer ${user.token}` },
+                    }
+                );
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch assignment');
+                }
+                const json = await response.json();
+                setIsLoading(false);
+                setAssignment(json.question);
+            } catch (error) {
+                setIsLoading(false);
+                setError(true);
+                console.log(error);
+            }
+        };
+
+        if (user) {
+            fetchAssignment();
+        }
+    }, [user, id, location.pathname, assignmentTopic, navigate]);
+
+    useEffect(() => {
+        // Navigate to the next or previous assignment when assignmentId changes
+        navigate(`/assignments/${topic}/${assignmentId}`);
+    }, [assignmentId, navigate, topic]);
+
+    const handlePrevAssignment = (e) => {
+        e.preventDefault();
+        setAssignmentId((prevId) => parseInt(prevId) - 1);
+    };
+
+    const handleNextAssignment = (e) => {
+        e.preventDefault();
+        setAssignmentId((prevId) => parseInt(prevId) + 1);
+    };
 
     return (
         <div className="mainAssignmentContainer">
-            <Header title={"Assignments"} />
-            <div className="openedAssignmentContainer">
-                <div className="prevAndNextBtn">
-                    <div className="prevContainer">
-                        <AiIcons.AiOutlineArrowLeft /> <h3 className="prev">Previous Assignment</h3>
+            {error && <div></div>}
+            {isLoading && (
+                <div className="loadingSpinnerContainer">
+                    <div className="loadingSpinner loadingAssignment"></div>
+                </div>
+            )}
+            {!isLoading && <Header title={assignment.title} />}
+            {!isLoading && (
+                <div className="openedAssignmentContainer">
+                    <div className="prevAndNextBtn">
+                        <div className="prevContainer" onClick={handlePrevAssignment}>
+                            <AiIcons.AiOutlineArrowLeft />{' '}
+                            <button className="prev">Previous Assignment</button>
+                        </div>
+                        <div className="nextContainer" onClick={handleNextAssignment}>
+                            <button className="next">Next Assignment</button>{' '}
+                            <AiIcons.AiOutlineArrowRight />
+                        </div>
                     </div>
-                    <div className="nextContainer">
-                        <h3 className="next">Next Assignment</h3> <AiIcons.AiOutlineArrowRight />
+                    <div className="assignmentTextBox">
+                        <div className="assignmentText">
+                            <p>{assignment.question}</p>
+                            <p>
+                                You are expected to finish this assignment in <b>1:30 hours.</b>{' '}
+                                Good luck and happy coding!
+                            </p>
+                        </div>
+                    </div>
+                    <div className="timerContainer">
+                        <Timer />
                     </div>
                 </div>
-                <div className="assignmentTextBox">
-                    <h2 className="assignmentTitle">{assignments[0].title}</h2>
-                    <div className="assignmentText">
-                        <p>{assignments[0].text}</p>
-                        <p>You are expected to finish this assignment in <b>1:30 hours.</b> Good luck and happy coding!</p>
-                    </div>
-                </div>
-                <div className="timerContainer">
-                    <Timer />
-                </div>
-            </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default OpenedAssignment
+export default OpenedAssignment;
