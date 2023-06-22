21d6da9700a2d6f150ca347970e15f528d6e6a3e
anastasialukanova <anastasia.lukanova@student.nhlstenden.com>
2023-06-22 13:46:15 +0200

    Add: made changes to size of flashcards

--- src/pages/lookThroughSets/components/Flashcard.js
+++ src/pages/lookThroughSets/components/Flashcard.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react'
+import { useAuthContext } from "../../../components/hooks/useAuthContext";
 
 export default function Flashcard({ flashcard }) {
     const [flip, setFlip] = useState(false)
+    const { user } = useAuthContext()
+
+    // const fetchFlashcards = async () => {
+    //     const response = await fetch(
+    //         `https://projectinnovate-it1e-backend-production.up.railway.app/flashcards/getDeck?deckId=`,
+    //         {
+    //             method: 'GET',
+    //             headers: { Authorization: `Bearer ${user.token}` },
+    //         }
+    //     );
+    //     const json = await response.json();
+    //
+    //     if (response.ok) {
+    //         fetchFlashcards(json.flashcards);
+    //     }
+    // };
+
+
+
+
     return (
         <div className={`card ${flip ? 'flip' : ''}`} onClick={() => setFlip(!flip)}>
             <div className="front">
