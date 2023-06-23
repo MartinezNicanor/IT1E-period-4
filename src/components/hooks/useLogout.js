c2ba4f94e593dee144b9dda0a937ee04ff1f6502
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2023-06-23 18:01:02 +0200

    Fixed: login and registration navigation

--- src/components/hooks/useLogout.js
+++ src/components/hooks/useLogout.js
@@ -1,12 +1,15 @@
 import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from 'react-router-dom'
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
+    const navigate = useNavigate()
 
     const logout = () => {
         //delete user from local storage
         localStorage.removeItem('user')
         dispatch({ type: 'LOGOUT' })
+        navigate('/')
     }
 
     return { logout }
